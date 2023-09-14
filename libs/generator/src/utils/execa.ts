5b5f42715c16cf2fa8b7e2b53684c48a0000c48f
Trần Công <trancong12102@gmail.com>
2023-09-14 11:49:03 +0700

    feat(generator/format): do not format files after save

--- libs/generator/src/utils/execa.ts
+++
@@ -1,8 +0,0 @@
-import { type execa as Execa } from 'execa';
-
-let execa: typeof Execa;
-eval(`import('execa')`).then((module: { execa: typeof Execa }) => {
-  execa = module.execa;
-});
-
-export { execa };
