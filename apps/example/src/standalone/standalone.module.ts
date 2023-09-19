2183b4f930a03eb38ee9f95ba8b3f0f2dc5b2335
Trần Công <trancong12102@gmail.com>
2023-09-19 14:11:56 +0700

    chore(example): prisma regenerate

--- apps/example/src/standalone/standalone.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StandaloneService } from './standalone.service';
-import { StandaloneResolver } from './standalone.resolver';
-
-@Module({
-  providers: [StandaloneService, StandaloneResolver],
-  exports: [StandaloneService],
-})
-export class StandaloneModule {}
